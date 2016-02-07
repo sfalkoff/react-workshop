8cd774f9b1d53b95402994115e7638534c7efb42
Sara Falkoff <sara@keen.io>
2016-02-06 16:22:27 -0800

    Styles and component lifecycle methods

--- src/lifecycle.js
+++ src/lifecycle.js
@@ -1,2 +1,26 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+
+var LifeCycle = React.createClass({
+  getInitialState: function() {
+    alert('getInitialState');
+    return {};  
+  },
+  componentWillMount: function() { 
+    alert('componentWillMount');
+  },
+  render: function() {
+    alert('render');
+    return (
+    <div> Hello World! </div>
+    );
+  },
+  componentDidMount: function() {
+    alert('componentDidMount');
+  }
+});
+
+ReactDOM.render(
+  <LifeCycle />,
+  document.getElementById('lifecycle')
+);
